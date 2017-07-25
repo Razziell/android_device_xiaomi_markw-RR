2e6fa61b6af51e708be96037d3429b09c564798e
Razziell <razziell@ya.ru>
2017-07-25 19:51:19 +0300

    Update device

--- device.mk
+++ device.mk
@@ -335,7 +335,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/fstman.ini:system/etc/wifi/fstman.ini \
-    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
 # default.prop
