3421f566b88e61024e1acc39f5e3383ff63441f5
Anomalchik <vidslava@yandex.ru>
2017-08-05 18:07:28 +0300

    Revert "audio: build audio hal from source"
    This reverts commit 0c0129803ae65a1dc8a260c3e84632392ea1a0a8.

--- device.mk
+++ device.mk
@@ -89,7 +89,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audiod \
     audio.a2dp.default \
-    audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
