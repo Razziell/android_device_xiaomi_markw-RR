c5e2ddc0f67c15e61c3315e8817fb43c6ce0b86b
Razziell <razziell@ya.ru>
2017-08-13 14:24:10 +0300

    Add perf-profile & update thermal-engine.conf

--- device.mk
+++ device.mk
@@ -250,6 +250,11 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/perf-profile0.conf:system/vendor/etc/perf-profile0.conf \
+    $(LOCAL_PATH)/configs/etc/perf-profile1.conf:system/vendor/etc/perf-profile1.conf
+
 # Power
 PRODUCT_PACKAGES += \
     power.msm8953
