dacf32154f0df24029cb04c45799a1f07db50007
Razziell <razziell@ya.ru>
2017-08-19 14:19:58 +0300

    Add QPerformance

--- device.mk
+++ device.mk
@@ -255,6 +255,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/etc/perf-profile0.conf:system/vendor/etc/perf-profile0.conf \
     $(LOCAL_PATH)/configs/etc/perf-profile1.conf:system/vendor/etc/perf-profile1.conf
 
+PRODUCT_BOOT_JARS += \
+       com.qulacomm.qti
+
 # Power
 PRODUCT_PACKAGES += \
     power.msm8953
